8e33524087f184eb7f04b667dc93f786286f47b7
minwo <mulee@neonexsoft.com>
2022-05-30 17:29:14 +0900

    sdgs
    sdhgdshg

--- my-app/src/components/Chart.js
+++ my-app/src/components/Chart.js
@@ -4,7 +4,70 @@ import React , { useEffect } from 'react';
 function canvas(){
     const el = document.querySelector('.chart');
     const ctx = el.getContext('2d');
-    console.log(el.clientWidth , el.clientHeight)
+    
+    let windowWidth = window.innerWidth;
+    
+    const x = 150;
+    const y = 150;
+    const radius = 100;
+    
+    let angleStart = 0;
+    let angleEnd = Math.PI * 1.7;
+
+    el.setAttribute('width',300);
+    el.setAttribute('height',300);
+
+    var gradient = ctx.createLinearGradient(0,500,0, 0);
+    gradient.addColorStop(0, '#2a62a9');
+    gradient.addColorStop(1, '#3dc8d5');
+
+    // ctx.beginPath();
+    // ctx.moveTo(x,y);
+    // ctx.arc(x,y,radius,angleStart,angleEnd, false);
+    // ctx.fillStyle = 'black';
+    // ctx.fill();
+    let gra = ctx.createLinearGradient(0,0,150, 150);
+    gra.addColorStop(0,'#2a62a9');
+    gra.addColorStop(.5,'#3392be');
+    gra.addColorStop(1,'#3dc8d5');
+    ctx.beginPath();
+    ctx.clearRect(0,0,x,y);
+    ctx.arc(x, x, radius, 0, 2*Math.PI, false);
+    ctx.strokeStyle = gra;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(x, x, radius, 0, 2*Math.PI, false);
+    ctx.lineWidth = 30;
+    // ctx.strokeStyle = 'rgba(0,0,0, 0.2)'
+    ctx.stroke();
+    
+    ctx.beginPath();
+    ctx.arc(x, y, radius, angleStart, 1.5);
+    ctx.strokeStyle = gradient;
+    ctx.lineWidth = 30;
+    ctx.stroke();
+    ctx.save();
+
+    // ctx.beginPath();
+    // ctx.arc(x, y, radius, 1.5, 2);
+    // ctx.strokeStyle = 'red';
+    // ctx.lineWidth = 30;
+    // ctx.stroke();
+ 
+    // ctx.beginPath();
+    // ctx.arc(x, y, radius, 2, 4);
+    // ctx.strokeStyle = 'blue';
+    // ctx.lineWidth = 30;
+    // ctx.stroke();
+
+    // ctx.beginPath();
+    // ctx.arc(x, y, radius, 4, 6.3);
+    // ctx.strokeStyle = 'blue';
+    // ctx.lineWidth = 30;
+    // ctx.stroke();
+
+
 }
 
 function Chart() {
@@ -14,7 +77,7 @@ function Chart() {
 
     return (
         <>
-            <canvas className='chart' style={{width:'200px',height:'200px',backgroundColor:'rgba(6,6,6,.3)'}}></canvas>
+            <canvas className='chart'></canvas>
         </>
     );
 }
